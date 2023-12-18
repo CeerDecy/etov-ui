0212ee1975a14df56b90b2613724c27d15d0c70e
CeerDecy <1748788674@qq.com>
2023-12-18 16:31:15 +0800

    feat: login page and register page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,107 @@
-'use client'
+import { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
 
-export default function Login() {
+import { cn } from "@/lib/utils"
+import { UserAuthForm } from "@/components/user-auth-form/user-auth-form"
+import {buttonVariants} from "@/components/ui/button";
+
+import "./index.css"
+
+export const metadata: Metadata = {
+    title: "Authentication",
+    description: "Authentication forms built using the components.",
+}
+
+export default function AuthenticationPage() {
     return (
-        <main>
-        </main>
+        <>
+            <div className="md:hidden">
+                <Image
+                    src="/examples/authentication-light.png"
+                    width={1280}
+                    height={843}
+                    alt="Authentication"
+                    className="block dark:hidden"
+                />
+                <Image
+                    src="/examples/authentication-dark.png"
+                    width={1280}
+                    height={843}
+                    alt="Authentication"
+                    className="hidden dark:block"
+                />
+            </div>
+            <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+                <Link
+                    href="/examples/authentication"
+                    className={cn(
+                        buttonVariants({ variant: "ghost" }),
+                        "absolute right-4 top-4 md:right-8 md:top-8"
+                    )}
+                >
+                    登录
+                </Link>
+                <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+                    <div className="absolute inset-0 bg-zinc-900"/>
+                    <div className="relative z-20 flex items-center  font-medium">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="mr-2 h-6 w-6"
+                        >
+                            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"/>
+                        </svg>
+                        welcome etovGPT
+                    </div>
+                    <div className={"relative z-20"}>
+                        <div className={"etov_bg"}>etov</div>
+                    </div>
+                    <div className="relative z-20 mt-auto">
+                        <blockquote className="space-y-2">
+                            <p className="">
+                                &ldquo;Learning to use GPT can quickly improve your learning and productivity!&rdquo;
+                            </p>
+                            <footer className="text-sm">CeerDecy</footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div className="lg:p-8">
+                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                        <div className="flex flex-col space-y-2 text-center">
+                            <h1 className="text-2xl font-semibold tracking-tight">
+                                创建属于你的账户
+                            </h1>
+                            <p className="text-sm text-muted-foreground">
+                                请在下方输入您的电子邮件以创建账户
+                            </p>
+                        </div>
+                        <UserAuthForm />
+                        <p className="px-8 text-center text-sm text-muted-foreground">
+                            点击继续，即表示您同意我们的{" "}
+                            <Link
+                                href="/terms"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                服务条款
+                            </Link>{" "}
+                            和{" "}
+                            <Link
+                                href="/privacy"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                隐私政策
+                            </Link>
+                            。
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
